7181f5f29ad91cc2e34745a95e43ff3a331753c8
SUPRIYA MISHRA <140587863+supiimishra01@users.noreply.github.com>
2025-06-02 14:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let imageArray = ['img1','img2','img3','img4','img5']
+
+const main = document.querySelector('main');
+let tile= document.createElement('img');
+
+imageArray.forEach(()=>{
+	
+})
